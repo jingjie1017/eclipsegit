e26b0521a5eb552ac66946136019ce28ba29c4be
jingjie <jingjie_jj@126.com>
2018-04-16 19:21:58 +0800

    第一次

---
+++ jing_jj/src/main/java/jing/jing_jj/App.java
@@ -0,0 +1,13 @@
+package jing.jing_jj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
